699172f53aab6887bdeb55d5da0201ab026be825
vaithiyanathan v <vaithiyanathan.veerappan@gmail.com>
2022-05-24 09:37:04 +0530

    multi line string

---
+++ Assignment_day1.py
@@ -0,0 +1,10 @@
+#multi line string
+Rhymes = '''Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are'''
+
+
+print(Rhymes)
